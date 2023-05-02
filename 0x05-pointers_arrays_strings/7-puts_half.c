a0eef526cb04160f0acaae3713c91aa57644bffd
Kenza LACHGAR <kenzalachgarensa@gmail.com>
2023-05-02 09:53:20 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - print a half of a string
+ * @str: string
+ * Return 0
+ */
+void puts_half(char *str)
+{
+	int i, n, lg;
+
+	lg = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		lg++;
+	n = (lg / 2);
+
+	if ((lg % 2) == 1)
+		n = ((lg + 1) / 2);
+
+	for (i = n; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}

7cd7c1f486e0d764a25568319103e14f477c836a
Kenza LACHGAR <kenzalachgarensa@gmail.com>
2023-05-18 04:15:14 -0700

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,10 +9,7 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-void errors(void);
-int is_digit(char *s);
 int _putchar(char c);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _strlen(char *s);
 
 #endif
